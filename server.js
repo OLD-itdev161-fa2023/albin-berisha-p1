5442218530a6c8c8064b03867a54d806ea7479ac
achooStorm <114879202+achooStorm@users.noreply.github.com>
2022-10-08 16:47:56 -0500

    Adding some more folders :)

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: "./config/config.env"});
+
+// Connect to database
+connectDB();
 
 // Route files
 const resortcamps = require('../albin-berisha-p1/routes/resortcamps');
 
-// Load env vars
-dotenv.config({ path: "./config/config.env"});
 
 const app = express();
 
@@ -20,9 +26,14 @@ app.use('/api/v1/resortcamps', resortcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
 
-
+// Handle unhandled promise rejctions
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
